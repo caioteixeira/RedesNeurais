6bfdde7927eed68ec9364ef7b3725a666d64d30f
ghn1712 <ghn1712@gmail.com>
2015-05-12 02:23:54 -0300

    Teste XOR

--- MLPTest.java
+++ MLPTest.java
@@ -1,14 +1,14 @@
 class MLPTest{
 	
 	public static void main(String[]args){
-		DataSet trainSet = new DataSet(-1, "optdigits.norm.cortado.tra");
+		DataSet trainSet = new DataSet(-1, "xor.tra");
 		trainSet.printClassDistribution();
-		DataSet testSet = new DataSet(-1, "optdigits.norm.cortado.tes");
+		DataSet testSet = new DataSet(-1, "xor.tes");
 		testSet.printClassDistribution();
-		DataSet validateSet = new DataSet(-1, "optdigits.norm.cortado.val");
+		DataSet validateSet = new DataSet(-1, "xor.val");
 		validateSet.printClassDistribution();
 		
-		Principal p = new Principal(1,61,9,40,false);
+		Principal p = new Principal(1,2,2,1,false);
 		for(int i = 0; i < 500;i++){
 			p.train(trainSet);
 		}
