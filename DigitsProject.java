010f8daa5ef5f7aee52ca1d7e8bcdba158c0fc7f
Joao Pedro Nardari dos Santos <joaonardari@gmail.com>
2015-05-13 10:02:46 -0300

    Help commands cmd

--- DigitsProject.java
+++ DigitsProject.java
@@ -1,6 +1,7 @@
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
@@ -9,23 +10,27 @@ public class DigitsProject {
 	
 	private static CommandLineParser parser = new DefaultParser();
 	private static Options options = new Options();
+	private static HelpFormatter formatter = new HelpFormatter();
 	
 	public static void main(String[] args) {
 		initializeOptions();
 		
-		// TODO Commands Parse
 		CommandLineParser parser = new DefaultParser();
 		try {
 			CommandLine cmd = parser.parse( options, args);
+			formatter.printHelp("DigitsProject", options, true);
 		} catch (ParseException e) {
-			e.printStackTrace();
+			//e.printStackTrace();
+			
+			System.out.println("Erro ao passar argumentos!");
+			formatter.printHelp("DigitsProject", options, true);
 		}
 	}
 	
 	private static void initializeOptions() {
-		options.addOption("tt", true, "nome do arquivo do conjunto de dados de treino");
+		options.addOption("tn", true, "nome do arquivo do conjunto de dados de treino");
 		options.addOption("vl", true, "nome do arquivo do conjunto de dados de validacao");
-		options.addOption("tn", true, "nome do arquivo do conjunto de dados de teste");
+		options.addOption("tt", true, "nome do arquivo do conjunto de dados de teste");
 		
 		options.addOption("lr", true, "taxa de aprendizado inicial");
 		
