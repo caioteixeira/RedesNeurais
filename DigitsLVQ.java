65db812dca176cb3c0232aa54f341d11befbb1d0
João Pedro Nardari dos Santos <joaonardari@terra.com.br>
2015-05-16 22:27:44 -0300

    Solving Imports

--- DigitsLVQ.java
+++ DigitsLVQ.java
@@ -1,8 +1,6 @@
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 public class DigitsLVQ extends Digits {
